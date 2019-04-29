d4f7de48ffb8058d4b80289bc73de48c89dd6d1f
Garik <Saghumyan>
2019-04-29 22:25:44 +0400

    Author : Garik Saghumyan : Added ragistration form

--- UploadFiles/src/main/java/am/instigate/upload_files/repository/ReadFileRepository.java
+++
@@ -1,9 +0,0 @@
-package am.instigate.upload_files.repository;
-
-import am.instigate.upload_files.entity.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReadFileRepository extends CrudRepository<User, Long> {
-}
