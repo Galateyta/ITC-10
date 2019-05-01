b3b297e9d119a2e8a75dedceeb4a687731bc019a
Anna Harutyunyan <anna.bk85@mail.ru>
2019-05-02 00:47:18 +0400

    Adding .html, .css, .js files

---
+++ Anna_Harutyunyan/HTML_CSS_HOMRWORK/script.js
@@ -0,0 +1,65 @@
+/* Functions for choosing skills */
+
+function range1() {
+	var
+	val = $('.range1').val();
+	$('.range1').css({'background':'-webkit-linear-gradient(left ,#fff 0%,#fff '+val+'%,#000 '+val+'%, #000 100%)'});
+ }
+
+ function range2() {
+	var
+	val = $('.range2').val();
+	$('.range2').css({'background':'-webkit-linear-gradient(left ,#fff 0%,#fff '+val+'%,#000 '+val+'%, #000 100%)'});
+ }
+
+ function range3() {
+	var
+	val = $('.range3').val();
+	$('.range3').css({'background':'-webkit-linear-gradient(left ,#fff 0%,#fff '+val+'%,#000 '+val+'%, #000 100%)'});
+ }
+
+ function range4() {
+	var
+	val = $('.range4').val();
+	$('.range4').css({'background':'-webkit-linear-gradient(left ,#fff 0%,#fff '+val+'%,#000 '+val+'%, #000 100%)'});
+ }
+
+ function range5() {
+	var
+	val = $('.range5').val();
+	$('.range5').css({'background':'-webkit-linear-gradient(left ,#fff 0%,#fff '+val+'%,#000 '+val+'%, #000 100%)'});
+ }
+
+ function range6() {
+	var
+	val = $('.range6').val();
+	$('.range6').css({'background':'-webkit-linear-gradient(left ,#fff 0%,#fff '+val+'%,#000 '+val+'%, #000 100%)'});
+ }
+
+ function range7() {
+	var
+	val = $('.range7').val();
+	$('.range7').css({'background':'-webkit-linear-gradient(left ,#fff 0%,#fff '+val+'%,#000 '+val+'%, #000 100%)'});
+ }
+
+ /* End (skills) */
+
+ /* Functions for choosing languages level -- */
+
+ function range_v1() {
+	var
+	val = $('.vertical-range1').val();
+	$('.vertical-range1').css({'background':'-webkit-linear-gradient(left ,#fff 0%,#fff '+val+'%,#000 '+val+'%, #000 100%)'});
+ }
+
+ function range_v2() {
+	var
+	val = $('.vertical-range2').val();
+	$('.vertical-range2').css({'background':'-webkit-linear-gradient(left ,#fff 0%,#fff '+val+'%,#000 '+val+'%, #000 100%)'});
+ }
+
+ function range_v3() {
+	var
+	val = $('.vertical-range3').val();
+	$('.vertical-range3').css({'background':'-webkit-linear-gradient(left ,#fff 0%,#fff '+val+'%,#000 '+val+'%, #000 100%)'});
+ }
