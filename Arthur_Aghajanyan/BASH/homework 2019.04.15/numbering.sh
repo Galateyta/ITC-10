237ed37c8f0d67a61ae3703978e9690ba5eebb1f
Armine <arm.gevorgyan94@gmail.com>
2019-05-03 06:22:32 +0400

    -Added Homeworks/2019.05.02

--- Arthur_Aghajanyan/BASH/homework 2019.04.15/numbering.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#run command 
-#   - bash numbering.sh <startLineNum> <endLineNum> <fileName>
-#   - bash numbering.sh 2 5 a.txt
-
-vim -esc''$1','$2's/^/\=printf("%d. ", line("."))/|x' $3
