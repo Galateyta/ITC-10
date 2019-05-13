0fb8a167272d07bc6314140e3d91a6791c3efd81
Tigran Nalbandyan <grig199922@inbox.ru>
2019-05-13 23:05:52 +0400

    Draw cards

--- Tigran_Nalbandyan/JavaScript/Durak/js/script.js
+++ Tigran_Nalbandyan/JavaScript/Durak/js/script.js
@@ -27,7 +27,7 @@ function getTrump(cards) {
 function getSixCards(cards) {
     const arr = [];
     for (let i = 0; i < 6; i++) {
-    	let card = getRandomItem(cards);
+        let card = getRandomItem(cards);
         arr.push(card)
         cards.remove(cards.indexOf(card));
     }
@@ -35,29 +35,45 @@ function getSixCards(cards) {
     return arr;
 }
 
-function draw (player1, player2, trump) {
-	const divPlayer1 = document.getElementById('player-1');
-	const length = player1.playerCards.length;
-	for (let i = 0; i < length; i++) {
-		let card = document.createElement('div');
-		card.className = 'card';
-		divPlayer1.appendChild(card);
-	}
+function drawPlayerCards(player) {
+    const divPlayer1 = document.getElementById(`player-${player.id}`);
+    const length = player.playerCards.length;
+    for (let i = 0; i < length; i++) {
+        let card = document.createElement('div');
+        card.className = 'card';
+        let span = document.createElement('span');
+        span.textContent = player.playerCards[i];
+        card.appendChild(span);
+        divPlayer1.appendChild(card);
+    }
+}
 
-	//TODO draw for player 2 and trump
+function drawTrump(trump) {
+    const card = document.getElementById('trump');
+    let span = document.createElement('span');
+    span.textContent = trump;
+    card.appendChild(span);
+}
+
+function drawCards(player1, player2, trump) {
+    drawPlayerCards(player1);
+    drawPlayerCards(player2);
+    drawTrump(trump);
 }
 
 function game() {
     let player1 = new Player(1);
     let player2 = new Player(2);
 
-    let cards = ["s1", "h1", "d1", "c1", "s6", "h6", "d6", "c6", "s7", "h7", "d7", "c7", "s8", "h8", "d8", "c8", "s9", "h9", "d9", "c9", "s10", "h10", "d10", "c10", "s11", "h11", "d11", "c11", "s12", "h12", "d12", "c12", "s13", "h13", "d13", "c13"];
+    let cards = ["♠ A", "♥ A", "♦ A", "♣ A", "♠ 6", "♥ 6", "♦ 6", "♣ 6", "♠ 7", "♥ 7", "♦ 7", "♣ 7", "♠ 8", "♥ 8", "♦ 8", "♣ 8", "♠ 9", "♥ 9", "♦ 9", "♣ 9", "♠ 10", "♥ 10", "♦ 10", "♣ 10", "♠ J", "♥ J", "♦ J", "♣ J", "♠ Q", "♥ Q", "♦ Q", "♣ Q", "♠ K", "♥ K", "♦ K", "♣ K"];
     const trump = getTrump(cards);
 
     player1.playerCards = getSixCards(cards);
     player2.playerCards = getSixCards(cards);
 
-    draw(player1, player2, trump);
+    drawCards(player1, player2, trump);
 
-    // implement game running
+		
 }
+
+game()
