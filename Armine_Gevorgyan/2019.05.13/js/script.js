8abdaa6b26b3ddeed12b00eab62b2c41c6ca1e13
Armine <arm.gevorgyan94@gmail.com>
2019-05-13 23:12:05 +0400

    -Add new file

---
+++ Armine_Gevorgyan/2019.05.13/js/script.js
@@ -0,0 +1,62 @@
+let cards = new Object(); //all cards
+let kozrs = new Object(); //kozrs
+let gamer1 = new Object();
+let gamer1Cnt = 0; //number cards of gamer1
+
+let gamer2 = new Object();
+let gamer2Cnt = 0; //number cards of gamer2
+
+let coloda = new Object();
+let colodaCnt = 0; //number cards of caloda
+
+let lastCard = -1; //last card
+let kozr = -1; 
+
+let numberCards = 36;
+
+for (let i=0; inumberCards; i++){
+	//TODO
+	/*
+	inicialization cards keys - value
+	key - 0-35
+	value 0-3
+	
+	*/
+}
+
+//genarate cards for first gamer
+function gamer1card(){
+	//TODO
+	/* 6 cards of 36 cards
+	copy cards object,
+	random 6 card give gamer1 and delete that cards 
+	*/
+}
+
+//genarate cards for second gamer
+function gamer2card(){
+	//TODO
+	/* 6 cards of rest 30 cards
+	copy cards object,
+	random 6 card give gamer1 and delete that cards 
+	*/
+}
+
+function caloda(){
+	//TODO
+	/* 
+	rest 24 cards
+	*/
+}
+
+
+//Generate kozr
+funtion kozr(){
+	//TODO
+	/*
+	random generate kozr 
+	*/
+}
+
+
+
