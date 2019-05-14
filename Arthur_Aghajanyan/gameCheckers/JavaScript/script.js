5c53dd24efd9e05222ab132a869fc8911ab4be9b
ArthurAghajanyan <arthur.aghajanyan.1994@mail.ru>
2019-05-15 03:01:05 +0400

    -Author Arthur Aghajanyan
            -Apdated game checkers

--- Arthur_Aghajanyan/gameCheckers/JavaScript/script.js
+++ Arthur_Aghajanyan/gameCheckers/JavaScript/script.js
@@ -1,10 +1,20 @@
 function start() {
     document.getElementById('start').style.display = 'none';
+    document.getElementById('playerStep').style.display = 'block';
+    document.getElementById('finishMove').style.display = 'block';
+    document.getElementById('chessboard').style.display = 'block';  
     createForm();
-
 }
 //char code for 3 elements
-
+let index = 1;
+function playerIndex(){
+    if(index % 2 === 1){
+        document.getElementById('playerStep').innerHTML = 'Black player step';
+    }else{
+        document.getElementById('playerStep').innerHTML = 'White player step';
+    }
+    index++;
+}
 const whiteFigureCharCode = '&#9920';
 const blackFigureCharCode = '&#9922';
 const emptyFigureCharCode = '&#8226';
@@ -39,7 +49,6 @@ var Figure = function(i,j,charCode,clickedIndex) {
     this.i = i;
     this.j = j;
     this.charCode = charCode;
-    this.clickedIndex = clickedIndex;
 }
 
 //created seter and geter for each members
@@ -62,12 +71,6 @@ Figure.prototype.getCharCode = function() {
 Figure.prototype.setCharCode = function(CharCode) {
     this.CharCode = CharCode;
 }
-Figure.prototype.getClickedIndex = function() {
-    return this.clickedIndex;
-}
-Figure.prototype.setClickedIndex = function(clickedIndex) {
-    this.clickedIndex = clickedIndex;
-}
 
 let whiteFigure = null;
 let blackFigure = null;
@@ -78,35 +81,29 @@ function clickFigure(element) {
 
     row = element.parentNode.rowIndex;      //parentNode for use row(tr)
     col = element.cellIndex;
+
     let id = row.toString() + col.toString();
 
     //create objects at the time of the click, using <getCharCodeById> function
-    if(getCharCodeById(id) == '9920'){
-        let index = blackFigure === null ? 1 : 2;
-        whiteFigure = new Figure(row,col,'9920',index);
+    if(getCharCodeById(id) == '9920' && (index % 2 === 1)){
+        whiteFigure = new Figure(row,col,'9920');
 
-    }else if(getCharCodeById(id) == '9922'){
-        let index = whiteFigure === null ? 1 : 2;
+    }else if(getCharCodeById(id) == '9922' && (index % 2 === 0)){
         blackFigure = new Figure(row,col,'9922');
 
     }else if((getCharCodeById(id) == '8226') && ((whiteFigure !== null) || (blackFigure !== null))){
         emptyFigure = new Figure(row,col,'8226');
     }
-
     //check to the first not to click click empti <td> , and as a result we call the corresponding function
 
-    if(((whiteFigure !== null) && (whiteFigure.getClickedIndex() === 1)) && ((blackFigure !== null) || (emptyFigure !== null))){
+    if((whiteFigure !== null) && (emptyFigure !== null)  ){
+        let temp = emptyFigure;
+        checkStepForWhiteFigure(whiteFigure,temp);
 
-        let temp = blackFigure !== null ? blackFigure : emptyFigure;
-        checkStepForWhiteFigure(whiteFigure,temp);             
-
-    }else if((blackFigure !== null) && ((whiteFigure !== null) || (emptyFigure !== null))){
-
-        let temp = whiteFigure !== null ? whiteFigure : emptyFigure;
+    }else if((blackFigure !== null) && (emptyFigure !== null) ){
+        let temp = emptyFigure;
         checkStepForBlackFigure(blackFigure,temp);
     }
-
-
 }
 
 function getCharCodeById(id){
@@ -114,18 +111,65 @@ function getCharCodeById(id){
 }
 
 //go for white figure
+function drawElementWhite(obj1I,obj1J,obj2I,obj2J){
+    document.getElementById(obj2I.toString() + obj2J.toString()).innerHTML = whiteFigureCharCode;
+    document.getElementById(obj1I.toString() + obj1J.toString()).innerHTML = emptyFigureCharCode;
+}
+//go for black figure
+function drawElementBlack(obj1I,obj1J,obj2I,obj2J){
+    document.getElementById(obj2I.toString() + obj2J.toString()).innerHTML = blackFigureCharCode;
+    document.getElementById(obj1I.toString() + obj1J.toString()).innerHTML = emptyFigureCharCode;
+}
 
 function checkStepForWhiteFigure(obj1,obj2) {
-
     let obj1I = obj1.getI();
     let obj1J = obj1.getJ();
     let obj2I = obj2.getI();
     let obj2J = obj2.getJ();
 
-    if((obj2I - obj1I) === 1 && (((obj2J - obj1J) === 1) || ((obj2J - obj1J) === -1))){
-        document.getElementById(obj2I.toString() + obj2J.toString()).innerHTML = whiteFigureCharCode;
-        document.getElementById(obj1I.toString() + obj1J.toString()).innerHTML = emptyFigureCharCode;
+    //variable for forward fire
+    let fireForwardI1 = obj1.getI()+1;
+    let fireForwardJ1 = obj1.getJ()+1;
+    let fireForwardJ2 = obj1.getJ()-1;
+    //variable for back fire
+    let fireWithI1 = obj1.getI()-1;
+    let fireWithJ1 = obj1.getJ()-1;
+    let fireWithJ2 = obj1.getJ()+1;
 
+    //move part
+
+    if((obj2I - obj1I) === 1 && (((obj2J - obj1J) === 1) || ((obj2J - obj1J) === -1))){
+        drawElementWhite(obj1I,obj1J,obj2I,obj2J);
+    }
+    //forward fire
+    if((obj2I - obj1I) === 2 && (obj2J - obj1J) === 2){
+        let id = fireForwardI1.toString() + fireForwardJ1.toString()
+            if(getCharCodeById(id) == '9922'){
+                drawElementWhite(obj1I,obj1J,obj2I,obj2J);
+                document.getElementById(fireForwardI1.toString() + fireForwardJ1.toString()).innerHTML = emptyFigureCharCode;
+            }
+    }
+    if((obj2I - obj1I) === 2 && (obj2J - obj1J) === -2){
+        let id = fireForwardI1.toString() + fireForwardJ2.toString()
+            if(getCharCodeById(id) == '9922'){
+                drawElementWhite(obj1I,obj1J,obj2I,obj2J);
+                document.getElementById(fireForwardI1.toString() + fireForwardJ2.toString()).innerHTML = emptyFigureCharCode;
+            }
+    }
+    //back fire
+    if((obj1I - obj2I) === 2 && (obj1J - obj2J) === 2){
+        let id = fireWithI1.toString() + fireWithJ1.toString()
+            if(getCharCodeById(id) == '9922'){
+                drawElementWhite(obj1I,obj1J,obj2I,obj2J);
+                document.getElementById(fireWithI1.toString() + fireWithJ1.toString()).innerHTML = emptyFigureCharCode;
+            }
+    }
+    if((obj1I - obj2I) === 2 && (obj2J - obj1J) === 2){
+        let id = fireWithI1.toString() + fireWithJ2.toString()
+            if(getCharCodeById(id) == '9922'){
+                drawElementWhite(obj1I,obj1J,obj2I,obj2J);
+                document.getElementById(fireWithI1.toString() + fireWithJ2.toString()).innerHTML = emptyFigureCharCode;
+            }
     }
     whiteFigure = null;
     blackFigure = null;
@@ -140,13 +184,49 @@ function checkStepForBlackFigure(obj1,obj2) {
     let obj1J = obj1.getJ();
     let obj2I = obj2.getI();
     let obj2J = obj2.getJ();
-
+    //variables for forward fire
+    let fireForwardI1 = obj1.getI()-1;
+    let fireForwardJ1 = obj1.getJ()-1;
+    let fireForwardJ2 = obj1.getJ()+1;
+    //variables for back fire
+    let fireWithI1 = obj1.getI()+1;
+    let fireWithJ1 = obj1.getJ()-1;
+    let fireWithJ2 = obj1.getJ()+1;
+    //move part
     if((obj1I - obj2I) === 1 && (((obj1J - obj2J) === 1) || ((obj1J - obj2J) === -1))){
-        document.getElementById(obj2I.toString() + obj2J.toString()).innerHTML = blackFigureCharCode;
-        document.getElementById(obj1I.toString() + obj1J.toString()).innerHTML = emptyFigureCharCode;
+        drawElementBlack(obj1I,obj1J,obj2I,obj2J);
+    }
+    //forward fire
+    if((obj1I - obj2I) === 2 && (obj1J - obj2J) === 2){
+        let id = fireForwardI1.toString() + fireForwardJ1.toString();
+        if(getCharCodeById(id) == '9920'){
+            drawElementBlack(obj1I,obj1J,obj2I,obj2J);
+            document.getElementById(fireForwardI1.toString() + fireForwardJ1.toString()).innerHTML = emptyFigureCharCode;
+        }
+    }
+    if((obj1I - obj2I) === 2 && (obj1J - obj2J) === -2){
+        let id = fireForwardI1.toString() + fireForwardJ2.toString()
+            if(getCharCodeById(id) == '9920'){
+                drawElementBlack(obj1I,obj1J,obj2I,obj2J);
+                document.getElementById(fireForwardI1.toString() + fireForwardJ2.toString()).innerHTML = emptyFigureCharCode;
+            }
+    }
+    //back fire
+    if((obj2I - obj1I) === 2 && (obj1J - obj2J) === 2){
+        let id = fireWithI1.toString() + fireWithJ1.toString()
+            if(getCharCodeById(id) == '9920'){
+                drawElementBlack(obj1I,obj1J,obj2I,obj2J);
+                document.getElementById(fireWithI1.toString() + fireWithJ1.toString()).innerHTML = emptyFigureCharCode;
+            }
+    }
+    if((obj2I - obj1I) === 2 && (obj2J - obj1J) === 2){
+        let id = fireWithI1.toString() + fireWithJ2.toString();
+        if(getCharCodeById(id) == '9920'){
+            drawElementBlack(obj1I,obj1J,obj2I,obj2J);
+            document.getElementById(fireWithI1.toString() + fireWithJ2.toString()).innerHTML = emptyFigureCharCode;
+        }
     }
     whiteFigure = null;
     blackFigure = null;
     emptyFigure = null;
 }
-
