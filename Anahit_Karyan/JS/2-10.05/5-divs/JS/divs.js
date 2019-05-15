30a784cd63aabd5fc5c604fb3619534685b1d208
AnahitKaryan <37872594+AnahitKaryan@users.noreply.github.com>
2019-05-15 17:20:09 +0400

    Update divs.js
    update

--- Anahit_Karyan/JS/2-10.05/5-divs/JS/divs.js
+++ Anahit_Karyan/JS/2-10.05/5-divs/JS/divs.js
@@ -1,8 +1,8 @@
-document.getElementById('div1').addEventListener("click", function() {
-   this.style.backgroundColor = "blue";
+document.getElementById('div1').addEventListener('click', function() {
+   this.style.backgroundColor = 'blue';
 });
-document.getElementById('div2').addEventListener("click", function(e) {
+document.getElementById('div2').addEventListener('click', function(e) {
    e.stopPropagation();
-   this.style.backgroundColor = "white";
+   this.style.backgroundColor = 'white';
 });
 
