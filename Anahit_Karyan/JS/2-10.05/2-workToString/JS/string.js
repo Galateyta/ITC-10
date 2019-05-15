377571529c9e7176a33e199aa4261744e7174133
AnahitKaryan <37872594+AnahitKaryan@users.noreply.github.com>
2019-05-15 17:14:52 +0400

    Update string.js
    update

--- Anahit_Karyan/JS/2-10.05/2-workToString/JS/string.js
+++ Anahit_Karyan/JS/2-10.05/2-workToString/JS/string.js
@@ -27,17 +27,17 @@ console.log(changeString(string));
 
 //...........................
 function start() {
-	const string = prompt("Enter your string");
+	const string = prompt('Enter your string');
 	const string2 = changeString(string);
 
-	const divs = document.getElementsByTagName("div");
+	const divs = document.getElementsByTagName('div');
 
-	const firstH4 = document.createElement("h4");
+	const firstH4 = document.createElement('h4');
 	const hText1 = document.createTextNode(string);
 	firstH4.appendChild(hText1);
 	divs[0].appendChild(firstH4);
 
-	const secondH4 = document.createElement("h4");
+	const secondH4 = document.createElement('h4');
 	const hText2 = document.createTextNode(string2);
 	secondH4.appendChild(hText2);
 	divs[1].appendChild(secondH4);
