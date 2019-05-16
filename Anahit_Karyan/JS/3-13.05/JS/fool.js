60660bc180f99ba3dcb2db592ea64510c7879eff
AnahitKaryan <1ahanit1@mail.ru>
2019-05-16 14:09:28 +0400

    change game

--- Anahit_Karyan/JS/3-13.05/JS/fool.js
+++ Anahit_Karyan/JS/3-13.05/JS/fool.js
@@ -1,13 +1,14 @@
 'use strict'
 
-let step = true;//um hertna
 let cardId = 0;
+let step = true;//um hertna
 const CardStandartSize = 6;
-
 const cardSection = document.getElementsByClassName('cardSection');
-
+const cardPlayer = document.getElementsByClassName('cardPlayer');
+const game = new Game();
+game.initializationCalod();
 function valueGenerator() { 
-	let cardValue = [6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 'V', 'V', 'V', 'V', 'D', 'D', 'D', 'D', 'K', 'K', 'K', 'K', 'T', 'T', 'T', 'T']; 
+	let cardValue = [6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 11, 11, 11, 11, 12, 12, 12, 12, 13, 13, 13, 13, 14, 14, 14, 14]; 
 	let result; 
 	const charactersLength = cardValue.length; 
 	const index = (Math.floor(Math.random() * charactersLength));
@@ -33,11 +34,44 @@ function createCard() {
 	return new Card();
 }
 
-function Game (player11, player22) {
+function Player (playerName) {
+	const name = playerName;
+	this.playerCards = [];
+	this.paintPlayerCards = function () {
+		for(let i = 0; i < CardStandartSize; ++i) {
+			const cardItem = paintCard(this.playerCards[i]);
+			name === 'player1' ? cardSection[0].appendChild(cardItem) : cardSection[2].appendChild(cardItem);
+		}
+	}
+	this.checkGo = function(card) {
+            if(game.cardsTable.length === 0) {
+    	       return true;
+            } else {
+            	const result = game.cardsTable.filter(function(item){
+            		return item.value == card.value;
+            	});        
+                if (result.length > 0) {
+            		return true;
+                }
+            } 	
+		return false;
+	}
+	this.checkCover = function(card) {
+        const result = (card.value > game.cardsTable[game.cardsTable.length - 1].value && card.holder === game.cardsTable[game.cardsTable.length - 1].holder) ||
+        				(card.holder === game.cozyr.holder  && card.holder !== game.cardsTable[game.cardsTable.length - 1].holder);
+        console.log('result='+  result)
+        return result;
+           
+	}
+}
+console.log(game.returnCalod());
+console.log(game.cozyr);
+function Game () {
 	const cardsSize = 36;
 	const cardsCalod = [];
-	const player1 = player11;
-	const player2 = player22;
+	this.player1 = new Player('player1');//skzbum gnacoxna
+	this.player2 = new Player('player2');//skzbum cackoxna
+	this.cardsTable = [];
 	this.cozyr;
 	this.initializationCalod = function () {	
 		for(let i = 0; i < cardsSize; ++i) {
@@ -50,51 +84,16 @@ function Game (player11, player22) {
 			});
 			isTrue === true ? cardsCalod[i] = carentCard : i--;
 		}
-		this.cozyr = cardsCalod.pop();
+		this.cozyr = cardsCalod.pop(); console.log(this.cozyr)
+		cardsCalod.unshift(this.cozyr);
 	}
-	//karcum em petq chi mka xarna generaxrel skzbum arden-------------------------------???????????
-	// this.shuffle = function() {
-	//     for (let i = this.cardsCalod.length - 1; i > 0; i--) {
-	//         let j = Math.floor(Math.random() * (i + 1));
-	//         let temp = this.cardsCalod[i];
-	//         this.cardsCalod[i] = this.cardsCalod[j];
-	//         this.cardsCalod[j] = temp;
-	//     }
-	//     return this.cardsCalod;
-	// }
-
 	this.giveCard = function (player) {
 		player.playerCards.push(cardsCalod.pop());
 	}
-	// this.returnCalod = function() {
-	// 	return cardsCalod ;
-	// }
-}
-
-function Player (playerName, comd) {
-	const name = playerName;
-	this.playerCards = [];
-	this.comdition = comd;//kargavichak (cackoxn es te gnacox@)
-	this.paintPlayerCards = function () {
-		for(let i = 0; i < CardStandartSize; ++i) {
-			const cardItem = paintCard(this.playerCards[i]);
-			name === 'player1' ? cardSection[0].appendChild(cardItem) : cardSection[2].appendChild(cardItem);
-		}
+	this.returnCalod = function(){
+		return cardsCalod;
 	}
-	// this.play = function() {
 }
-
-function cardClick (e) {
-	const cardInHtml = e.target; 
- 	const id = cardInHtml.id;
- 	const cardInJs = document.getElementById(e.target.id);
-	cardSection[1].appendChild(cardInJs);
- 	//if(id.parentElement.id === p1-section comdition ===1 ) gnacoxn es voch te cackox@
- 	const divPlayer1 = document.createElement('div');
- 	divPlayer1.appendChild(cardInJs);
- 	cardSection[1].appendChild(divPlayer1);
- 	let step = false;
- }
  //id tvox funkcia
 function giveIdAndOnClick (element , id) {
 	element.setAttribute('onclick', 'cardClick(event)');
@@ -105,7 +104,24 @@ function paintCard(card) {
 	cardDiv.setAttribute('class', 'card shadow  bg-white m-3 myCards'); 
 	giveIdAndOnClick (cardDiv, card.id);
 	const h3 = document.createElement('h3');
-	const hText = document.createTextNode(card.value);
+	let hText;
+	switch(card.value){
+	  case '11':
+	    hText = document.createTextNode('V');
+	    break;
+	  case '12':
+	    hText = document.createTextNode('D');
+	    break;
+	  case '12':
+	   hText = document.createTextNode('K');
+	    break;
+	  case '14':
+	   hText = document.createTextNode('T');
+	    break;
+	  default:
+	    hText = document.createTextNode(card.value);
+        break;
+	} 
 	h3.appendChild(hText);
 	h3.setAttribute('class', 'text-left'); 
 	giveIdAndOnClick (h3, card.id);//qani vor iran click aneluc cher ashxatum clicki funkcian
@@ -134,63 +150,152 @@ function paintCard(card) {
 	return cardDiv;
 }
 
+//......start..........................................................................................
 
 
-
-
-//starti meja gnalu
-
-
-const player1 = new Player('player1', 1);//xaxi skzbum gnacoxna
-const player2 = new Player('player2', 0);//xaxi skzbum cackoxna
-const game = new Game(player1, player2);
-
-function calodClick() {
-	if(step ) {
-		//while(player1.playerCards.length !== 5)
-		game.giveCard(player1);
-		const cardItem = paintCard(player1.playerCards[player1.playerCards.length - 1]);
-		cardSection[0].appendChild(cardItem);
-	} else {
-		//while(player2.playerCards.length !== 5)
-		game.giveCard(player2);
-		const cardItem = paintCard(player2.playerCards[player2.playerCards.length - 1]);
-		cardSection[2].appendChild(cardItem);
-	}
-
-}
-
-function collect () {	
-	if(step) {//aysinqn arajinn er gnacox erkrodn havaqelua
-		while (cardSection[1].firstChild) {//cardSection[1].removeChild(cardSection[1].firstChild);
-	    	cardSection[2].appendChild(cardSection[1].firstChild)
-		}
-	} else {
-		while (cardSection[1].firstChild) {
-			cardSection[0].appendChild(cardSection[1].firstChild)
-		}
-	}
-
-}
-function bita () {
-	while (cardSection[1].firstChild) {
-    	cardSection[1].removeChild(cardSection[1].firstChild);
-	}
-}
-
-function start () {
-	
-	game.initializationCalod();
 	for(let i = 0; i < CardStandartSize; ++i) {
-		game.giveCard(player1);
-		game.giveCard(player2);
+		game.giveCard(game.player1);
+		game.giveCard(game.player2);
 	}
-	player1.paintPlayerCards();
-	player2.paintPlayerCards();
+	game.player1.paintPlayerCards();
+	game.player2.paintPlayerCards();
 
 	const cardCozyr = paintCard(game.cozyr);
 	const divCozyr = document.getElementsByClassName('calod')[0];
 	divCozyr.appendChild(cardCozyr);
+
+//.......................................................................................................
+console.log(game.player1.playerCards)
+console.log(step)
+function cardClick (e) {
+	const cardInHtml = e.target; 
+ 	const id = cardInHtml.id;
+ 	const cardInJs = document.getElementById(id);
+	let carentCard;
+	let carentIndex;
+
+
+ 	if(step){console.log(1111111)
+ 		console.log(game.player1.playerCards)
+ 		carentCard = game.player1.playerCards.find(function(item, index) {
+ 			//carentIndex = index;
+
+ 			return item.id == id;
+ 		});	
+ 		if (!carentCard) { 
+ 			//alert("Hert@ myus xaxacoxinn e");
+ 			return;
+ 		}
+	    if((game.cardsTable.length % 2 === 0 && game.player1.checkGo(carentCard)) ||
+	    	(game.cardsTable.length % 2 != 0 && game.player1.checkCover(carentCard))
+	    	){
+	 	    cardPlayer[0].appendChild(cardInJs);
+	 		game.cardsTable.push(carentCard);
+	 		game.player1.playerCards.splice(carentIndex, 1);
+	 		step = !step;	console.log(step)
+	 	} 
+	 	
+ 	} else { 
+ 		console.log(22222222);console.log(game.player1.playerCards)
+ 		carentCard = game.player2.playerCards.find(function(item, index) {
+ 			//carentIndex = index;
+ 			return item.id == id;
+ 		});
+ 		if (!carentCard) { 
+ 			//alert("Hert@ myus xaxacoxinn e");
+ 			return;
+ 		}
+	 	if((game.cardsTable.length % 2 === 0 && game.player2.checkGo(carentCard)) ||
+	    	(game.cardsTable.length % 2 != 0 && game.player2.checkCover(carentCard))
+	    	){
+	 	    cardPlayer[1].appendChild(cardInJs);
+	 		game.cardsTable.push(carentCard);
+	 		game.player2.playerCards.splice(carentIndex, 1);
+	 		step = !step;	console.log(step)
+	 	}	
+	 	
+ 	} 
+ 	gameOver();
+ 	//step = !step;
+ 	 //console.log(step)
+ }
+ function gameOver(){
+ 	if (game.player1.playerCards.length === 0 && game.cardsCalod.length === 0) {
+ 		alert("Haxtec player1");
+ 	} else if (game.player2.playerCards.length === 0 && game.cardsCalod.length === 0) {
+ 		alert("Haxtec player2");
+ 	}
+ }
+
+function calodClick() {
+	console.log('player1-i chap ' + game.player1.playerCards.length);
+		console.log('player2-i chap ' + game.player2.playerCards.length);
+	//if(step ) {
+		if(game.cardsTable.length  === 0) {
+			while(game.player1.playerCards.length < 6) {
+				game.giveCard(game.player1);
+				const cardItem = paintCard(game.player1.playerCards[game.player1.playerCards.length - 1]);
+				cardSection[0].appendChild(cardItem);
+			}
+		//} else {
+			while(game.player2.playerCards.length < 6) {
+				game.giveCard(game.player2);
+				const cardItem = paintCard(game.player2.playerCards[game.player2.playerCards.length - 1]);
+				cardSection[2].appendChild(cardItem);
+			}
+	    }
+	//}
+
+}
+
+function collect () {	
+	if(game.cardsTable.length % 2 === 0) {
+		return;
+	}
+	if(!step) {//aysinqn arajinn er gnacox erkrodn havaqelua
+		while (cardPlayer[0].firstChild) {//cardSection[1].removeChild(cardSection[1].firstChild);
+	    	cardSection[2].appendChild(cardPlayer[0].firstChild);
+	    	game.player2.playerCards.concat(game.cardsTable);
+		}
+		while (cardPlayer[1].firstChild) {
+			cardSection[0].appendChild(cardPlayer[1].firstChild);
+			game.player1.playerCards.concat(game.cardsTable);
+		}
+		game.cardsTable.length = 0;
+		step = !step;
+	} else {
+		while (cardPlayer[0].firstChild) {
+			cardSection[0].appendChild(cardPlayer[0].firstChild);
+			game.player1.playerCards.concat(game.cardsTable);
+		}
+		while (cardPlayer[1].firstChild) {
+			cardSection[0].appendChild(cardPlayer[1].firstChild);
+			game.player1.playerCards.concat(game.cardsTable);
+		}
+		game.cardsTable.length = 0;
+		step = !step;
+	}
+    calodClick();
+}
+function bita () { 
+	if(game.cardsTable.length % 2 !== 0) {
+		return;
+	}
+	step = !step;
+	console.log(step)
+	while (cardPlayer[0].firstChild) {
+    	cardPlayer[0].removeChild(cardPlayer[0].firstChild);
+	}
+	while (cardPlayer[1].firstChild) {
+    	cardPlayer[1].removeChild(cardPlayer[1].firstChild);
+	}
+	game.cardsTable.length = 0;
+	calodClick();
+}
+
+function start () {
+	
+	
 
 }
 
