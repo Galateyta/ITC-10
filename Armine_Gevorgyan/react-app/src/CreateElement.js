b8976db8b5c2bada966409d21f98830dcbb7426f
Armine <arm.gevorgyan94@gmail.com>
2019-05-23 01:01:04 +0400

    -Modifired CreateElement.js

--- Armine_Gevorgyan/react-app/src/CreateElement.js
+++ Armine_Gevorgyan/react-app/src/CreateElement.js
@@ -1,11 +1,12 @@
-import React from 'react';
+iimport React from 'react';
  
 function CreateElement (props) {
   const text = props.text;
   const color = props.color;  
   return (
-<div>
-    <h1 style={{"color": color}} > {text} {color}</h1> </div>
+    <div>
+      <h1 style={{"color": color}} > {text} {color} </h1> 
+    </div>
   );
 } 
 
