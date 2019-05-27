a1fd7096c15409ffc3a19b8bfcd0fa2bb8ab151e
Mane <maneantonyan@gmail.com>
2019-05-27 23:56:56 +0400

    Added login page

---
+++ Mane_Antonyan/form/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Login from './form.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Login></Login>
+    </div>
+  );
+}
+
+export default App;
