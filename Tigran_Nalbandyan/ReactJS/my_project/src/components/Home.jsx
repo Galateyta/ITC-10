41219bb380a5a34acc13e725a0d53819038d00e9
Tigran Nalbandyan <grig199922@inbox.ru>
2019-05-28 22:40:44 +0400

    Add menu icon button

--- Tigran_Nalbandyan/ReactJS/my_project/src/components/Home.jsx
+++ Tigran_Nalbandyan/ReactJS/my_project/src/components/Home.jsx
@@ -1,20 +1,25 @@
 import React, {Component} from 'react';
-import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
-import Button from '@material-ui/core/Button';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
+import {
+  SwipeableDrawer,
+  List,
+  Divider,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  IconButton,
+  Box
+} from '@material-ui/core';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
+import MenuIcon from '@material-ui/icons/Menu';
 
-class SwipeableTemporaryDrawer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {open: false}
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
     }
-
+  }
 
   toggleDrawer = (open) => event => {
     if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
@@ -54,48 +59,28 @@ class SwipeableTemporaryDrawer extends Component {
     </div>
   );
 
-  fullList = side => (
-    <div
-      className="fullList"
-      role="presentation"
-      onClick={this.toggleDrawer(false)}
-      onKeyDown={this.toggleDrawer(false)}>
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0
-                ? <InboxIcon/>
-                : <MailIcon/>}</ListItemIcon>
-            <ListItemText primary={text}/>
-          </ListItem>
-        ))}
-      </List>
-      <Divider/>
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0
-                ? <InboxIcon/>
-                : <MailIcon/>}</ListItemIcon>
-            <ListItemText primary={text}/>
-          </ListItem>
-        ))}
-      </List>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <Box ml={2} mt={1}>
+          <IconButton
+            color="black"
+            aria-label="Open drawer"
+            onClick={this.toggleDrawer(true)}
+            edge="start">
+            <MenuIcon/>
+          </IconButton>
+        </Box>
 
-  render() {return (
-    <div>
-      <Button onClick={this.toggleDrawer(true)}>Open Left</Button>
-      <SwipeableDrawer
-        open={this.state.open}
-        onClose={this.toggleDrawer(false)}
-        onOpen={this.toggleDrawer(true)}>
-        {this.sideList('left')}
-      </SwipeableDrawer>
-    </div>
-  );
-}
+        <SwipeableDrawer
+          open={this.state.open}
+          onClose={this.toggleDrawer(false)}
+          onOpen={this.toggleDrawer(true)}>
+          {this.sideList('left')}
+        </SwipeableDrawer>
+      </div>
+    );
+  }
 }
 
-export default SwipeableTemporaryDrawer;
+export default Home;
