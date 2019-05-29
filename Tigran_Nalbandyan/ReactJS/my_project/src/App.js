f4c1654a0124bfe2a6d074d5b99569783a43fe9b
Tigran Nalbandyan <grig199922@inbox.ru>
2019-05-29 12:52:02 +0400

    Add log out, data passing to home and table from old homework

--- Tigran_Nalbandyan/ReactJS/my_project/src/App.js
+++ Tigran_Nalbandyan/ReactJS/my_project/src/App.js
@@ -4,31 +4,42 @@ import './App.css';
 import Login from './components/Login';
 import Register from './components/Register';
 import Home from './components/Home';
+import Table from './components/table/MyTable.jsx';
 
 class App extends Component {
   state = {
-    isAuthed: false,
+    isAuthed: true, //false
     users: [
       {
         login: 'admin',
         password: 'admin123',
         name: 'Admin',
+        surname: 'Adminich',
         dateOfBirthday: Date('19/03/1999'),
         gender: 'male',
         image: ''
       }
-    ]
+    ],
+    currentUser: {
+      login: 'admin',
+      password: 'admin123',
+      name: 'Admin',
+      surname: 'Adminich',
+      dateOfBirthday: Date('19/03/1999'),
+      gender: 'male',
+      image: ''
+    }
   }
 
   checkUser = (currentUser) => {
     const users = this.state.users;
     for (const user of users) {
       if (user.login === currentUser.login && user.password === currentUser.password) {
-        this.setState({isAuthed: true});
+        this.setState({isAuthed: true, currentUser: user});
         return true;
       }
     }
-    this.setState({isAuthed: false});
+    this.setState({isAuthed: false, currentUser: {}});
     return false;
   }
 
@@ -36,6 +47,11 @@ class App extends Component {
     const newUsers = this.state.users;
     newUsers.push(newUser);
     this.setState({users: newUsers});
+  }
+
+  logOut = () => {
+    console.log('log out')
+    this.setState({isAuthed: false, currentUser: {}});
   }
 
   render() {
@@ -52,8 +68,8 @@ class App extends Component {
             path='/'
             exact
             render={(props) => this.state.isAuthed
-            ? <Home {...props}/>
-            : <Login {...props} checkUser={this.checkUser}/>}/>
+            ? <Home {...props} currentUser={this.state.currentUser} logOut={this.logOut} component={<Table addUser={this.addUser}/>} />
+            : <Login {...props} checkUser={this.checkUser} />}/>
         </div>
       </BrowserRouter>
     );
--- Tigran_Nalbandyan/ReactJS/my_project/src/components/Home.jsx
+++ Tigran_Nalbandyan/ReactJS/my_project/src/components/Home.jsx
@@ -7,36 +7,59 @@ import {
   ListItemIcon,
   ListItemText,
   IconButton,
-  Box
+  Box,
+  Grid
 } from '@material-ui/core';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
 import MenuIcon from '@material-ui/icons/Menu';
+import ExitIcon from '@material-ui/icons/ExitToApp';
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      open: false
+      drawerOpen: false
     }
+    this.user = this.props.currentUser;
+    this.component = this.props.component;
   }
 
-  toggleDrawer = (open) => event => {
+  toggleDrawer = (drawerOpen) => event => {
     if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
       return;
     }
 
-    this.setState({open});
+    this.setState({drawerOpen});
   };
 
-  sideList = side => (
+  ImageAvatar(url) {
+    return (
+      <Grid container justify="center" alignItems="center">
+        <img
+          width="82px"
+          height="82px"
+          alt="Remy Sharp"
+          src="https://media.istockphoto.com/vectors/woman-icon-flat-single-avatarpeaople-icon-from-the-big-avatar-flat-vector-id1023960304?s=170x170"
+          className="big-avatar"/>
+      </Grid>
+    );
+  }
+
+  sideList = () => (
     <div
       className="list"
       role="presentation"
       onClick={this.toggleDrawer(false)}
       onKeyDown={this.toggleDrawer(false)}>
+      <div className="list-header">
+        <this.ImageAvatar/>
+        <h4>{this.user.name} {this.user.surname}</h4>
+        <span className="login-text">{this.user.login}</span>
+      </div>
+      <Divider/>
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+        {['Slider', 'About', 'Table'].map((text, index) => (
           <ListItem button key={text}>
             <ListItemIcon>{index % 2 === 0
                 ? <InboxIcon/>
@@ -47,14 +70,13 @@ class Home extends Component {
       </List>
       <Divider/>
       <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0
-                ? <InboxIcon/>
-                : <MailIcon/>}</ListItemIcon>
-            <ListItemText primary={text}/>
-          </ListItem>
-        ))}
+        <ListItem button key="Log Out" onClick={this.props.logOut}>
+          <ListItemIcon>
+            <ExitIcon/>
+          </ListItemIcon>
+          <ListItemText primary="Log Out"/>
+        </ListItem>
+
       </List>
     </div>
   );
@@ -62,18 +84,21 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <Box ml={2} mt={1}>
-          <IconButton
-            color="black"
-            aria-label="Open drawer"
-            onClick={this.toggleDrawer(true)}
-            edge="start">
-            <MenuIcon/>
-          </IconButton>
-        </Box>
+        <Grid container direction="row">
+          <Box ml={2} mt={1}>
+            <IconButton
+              color="default"
+              aria-label="Open drawer"
+              onClick={this.toggleDrawer(true)}
+              edge="start">
+              <MenuIcon/>
+            </IconButton>
+          </Box>
+          {this.component ? this.component : null}
+        </Grid>
 
         <SwipeableDrawer
-          open={this.state.open}
+          open={this.state.drawerOpen}
           onClose={this.toggleDrawer(false)}
           onOpen={this.toggleDrawer(true)}>
           {this.sideList('left')}
