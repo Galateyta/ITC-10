30b7573e2ae45c9da803d38f01e8833af15c667f
AnahitKaryan <1anahit1@mail.ru>
2019-05-30 18:27:41 +0400

    update coments

--- Anahit_Karyan/React/src/Homeworks/1-22.05/Text.js
+++ Anahit_Karyan/React/src/Homeworks/1-22.05/Text.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react';
 
 class Text extends Component {
-	 constructor(props) {
-    	super(props);
-		this.state = {
-			text: props.text,
-	        color: props.color
-	    };
-	}
     render() {
         return (
-            <p style={{color:this.state.color}}>
-               {this.state.text}
+            <p style={{color:this.props.color}}>
+               {this.props.text}
             </p>
         );
     }
 }
-
 
 export default Text;
