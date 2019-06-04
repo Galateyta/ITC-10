814ef6969a56f476895ba7d5d023fa85c1277bda
NarekJilavyan <45066634+NarekJilavyan@users.noreply.github.com>
2019-06-04 14:49:34 +0400

    fix Table.jsx error

--- Narek_Jilavyan/React/2019.06.03/src/components/Table.jsx
+++ Narek_Jilavyan/React/2019.06.03/src/components/Table.jsx
@@ -1,8 +1,4 @@
 import React, { Component } from 'react';
-import Inputs from './tablecomponents/Inputs';
-import Tables from './tablecomponents/Tables';
-import Filter from './tablecomponents/Filter';
-import { Col, Container, Row } from 'reactstrap';
 
 class Table extends Component {
   render() {
