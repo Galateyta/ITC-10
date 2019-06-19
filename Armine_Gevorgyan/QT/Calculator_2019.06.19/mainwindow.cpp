5fe3b28247721d3ab54eb90cc4bc526a90b77abd
Armine <arm.gevorgyan94@gmail.com>
2019-06-20 03:00:54 +0400

    -Add 2019.06.19 homework Armine_Gevorgyan

---
+++ Armine_Gevorgyan/QT/Calculator_2019.06.19/mainwindow.cpp
@@ -0,0 +1,113 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <iostream>
+
+
+double  calcVal = 0.0;
+
+bool divTrigger = false;
+bool multTrigger = false;
+bool addTrigger = false;
+bool subTrigger = false;
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->Display->setText(QString::number(calcVal));
+    QPushButton *numButtons[10];
+
+    for(int i = 0; i < 10; ++i){
+            QString butName = "Button" + QString::number(i);
+            numButtons[i] = MainWindow::findChild<QPushButton *>(butName);
+            connect(numButtons[i], SIGNAL(released()), this, SLOT(NumPressed()));
+    }
+
+    connect(ui->Add, SIGNAL(released()), this, SLOT(MathButtonPressed()));
+    connect(ui->Subtract, SIGNAL(released()), this, SLOT(MathButtonPressed()));
+    connect(ui->Multiply, SIGNAL(released()), this, SLOT(MathButtonPressed()));
+    connect(ui->Divide, SIGNAL(released()), this, SLOT(MathButtonPressed()));
+    connect(ui->Equals, SIGNAL(released()), this, SLOT(EqualButtonPressed()));
+    connect(ui->Clear, SIGNAL(released()), this, SLOT(Clear()));
+
+
+}
+
+
+//calculator - > MainWindow
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+//Clear display
+void MainWindow::Clear()
+{
+    double a = 0.0;
+    ui->Display->setText(QString::number(a, 'g', 16));
+}
+
+//press number
+void MainWindow::NumPressed()
+{
+    QPushButton *button = (QPushButton *)sender();
+    QString butVal = button->text();
+    QString displayVal = ui->Display->text();
+
+    if((displayVal.toDouble() == 0) || (displayVal.toDouble() == 0.0)){
+        ui->Display->setText(butVal);
+    } else {
+        QString newVal = displayVal + butVal;
+        double dblNewVal = newVal.toDouble();
+        ui->Display->setText(QString::number(dblNewVal, 'g', 16));
+    }
+}
+
+//Calculate
+void MainWindow::MathButtonPressed(){
+    divTrigger = false;
+    multTrigger = false;
+    addTrigger = false;
+    subTrigger = false;
+
+    QString displayVal = ui->Display->text();
+    calcVal = displayVal.toDouble();
+    QPushButton *button = (QPushButton *)sender();
+    QString butVal = button->text();
+
+    if(QString::compare(butVal, "/", Qt::CaseInsensitive) == 0){
+        divTrigger = true;
+    } else if(QString::compare(butVal, "*", Qt::CaseInsensitive) == 0){
+        multTrigger = true;
+    } else if(QString::compare(butVal, "+", Qt::CaseInsensitive) == 0){
+        addTrigger = true;
+    } else {
+        subTrigger = true;
+    }
+    ui->Display->setText("");
+}
+
+
+void MainWindow::EqualButtonPressed(){
+    double solution = 0.0;
+
+    QString displayVal = ui->Display->text();
+    double dblDisplayVal = displayVal.toDouble();
+
+    if(addTrigger || subTrigger || multTrigger || divTrigger ){
+        if(addTrigger){
+            solution = calcVal + dblDisplayVal;
+        } else if(subTrigger){
+            solution = calcVal - dblDisplayVal;
+        } else if(multTrigger){
+            solution = calcVal * dblDisplayVal;
+        } else {
+            solution = calcVal / dblDisplayVal;
+        }
+    }
+
+    ui->Display->setText(QString::number(solution));
+
+}
