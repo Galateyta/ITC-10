6549f34bca96d9dafe92c10b7113b8871ffff73d
HunanKhachatryan <hunank@mail.ru>
2019-06-24 22:39:46 +0400

    Author Hunan Khachatryan Downloader

--- Hunan_Khachatryan/Downloader/mainwindow.cpp
+++ Hunan_Khachatryan/Downloader/mainwindow.cpp
@@ -1,11 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include <QNetworkAccessManager>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
 }
 
 MainWindow::~MainWindow()
@@ -18,11 +19,13 @@ void MainWindow::on_pushButton_clicked()
     QNetworkAccessManager *nam = new QNetworkAccessManager(this);
         connect(nam, &QNetworkAccessManager::finished, this, &MainWindow::downloadFinished);
 
+
         QString link = ui->lineEdit->text();
         const QUrl url = QUrl(link);
         QNetworkRequest request(url);
         nam->get(request);
 }
+
 void MainWindow::downloadFinished(QNetworkReply *reply)
 {
     QPixmap pm;
