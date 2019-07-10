c8535141c2bd414dc7f196e08b5a9b4096257f5a
HunanKhachatryan <hunank@mail.ru>
2019-07-10 20:51:04 +0400

    -Author Hunan Khachatryan

---
+++ Hunan_Khachatryan/Browser/Browser/downloadmanager.h
@@ -0,0 +1,30 @@
+#ifndef DOWNLOADMANAGER_H
+#define DOWNLOADMANAGER_H
+
+#include <QNetworkAccessManager>
+
+class DownloadManager : public QObject
+{
+    Q_OBJECT
+public:
+    DownloadManager(QObject* parent);
+    ~DownloadManager();
+
+    void start(QString url, void* usrPtr);
+    QByteArray serverRun();
+    void clientRun(QString url);
+    void startImage(QString url, void* usrPtr);
+
+signals:
+    void finished(void* usrPtr, QByteArray data);
+    void imageFinished(void* usrPtr, QByteArray data);
+    void xmlfinished(void* usrPtr, QByteArray data);
+
+private slots:
+    void slotDownloadFinished(QNetworkReply* reply);
+
+private:
+    QNetworkAccessManager manager;
+};
+
+#endif // DOWNLOADMANAGER_H
