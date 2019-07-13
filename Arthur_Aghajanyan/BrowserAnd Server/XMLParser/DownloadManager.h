9768d450e9f98901f71b254de3c14400e770f095
Arthur Aghajanyan <arthur.aghajanyan.1994@mail.ru>
2019-07-13 11:49:55 +0400

    -Author Arthur Aghajanyan
            -Finished Browser and Server

---
+++ Arthur_Aghajanyan/BrowserAnd Server/XMLParser/DownloadManager.h
@@ -0,0 +1,31 @@
+#ifndef DOWNLOAD_H
+#define DOWNLOAD_H
+#include <QNetworkAccessManager>
+
+class DownloadManager : public QObject
+{
+    Q_OBJECT
+public:
+    DownloadManager(QObject* parent);
+    ~DownloadManager();
+
+    void start(QString url, void* usrPtr);
+
+    QByteArray serverRun();
+    void clientRun(QString url);
+    void startImage(QString url, void* usrPtr);
+    void p(QString url);
+    QByteArray startImgSendServer(QString url);
+signals:
+    void finished(void* usrPtr, QByteArray data);
+    void xmlfinished(void* usrPtr, QByteArray data);
+
+private slots:
+    void slotDownloadFinished(QNetworkReply* reply);
+
+private:
+    QNetworkAccessManager manager;
+
+};
+
+#endif // DOWNLOAD_H
