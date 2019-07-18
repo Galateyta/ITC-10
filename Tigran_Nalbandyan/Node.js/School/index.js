c762ec2fb72292da74927075a33d4017b82e11db
Tigran Nalbandyan <grig199922@inbox.ru>
2019-07-18 22:53:31 +0400

    Add get by query and use of express-validator

--- Tigran_Nalbandyan/Node.js/School/index.js
+++ Tigran_Nalbandyan/Node.js/School/index.js
@@ -1,33 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const { check, oneOf, validationResult } = require('express-validator');
+  
 const app = express();
 const port = 8000;
 
-let students = [
-  {
-    id: 1,
-    name: 'Narek',
-    age: '17',
-    grade: '10A'
-  }, {
-    id: 2,
-    name: 'Mane',
-    age: '19',
-    grade: '12B'
-  }
-];
-let teachers = [
-  {
-    id: 3,
-    name: 'Tigran',
-    age: '16'
-  }, {
-    id: 4,
-    name: 'Garik',
-    age: '27'
-  }
-];
+let students = [{
+  id: 1,
+  firstname: 'Narek',
+  lastname: 'Jilavyan',
+  age: '17',
+  grade: '10A'
+}, {
+  id: 2,
+  firstname: 'Mane',
+  lastname: 'Antonyan',
+  age: '19',
+  grade: '12B'
+}];
+let teachers = [{
+  id: 3,
+  name: 'Tigran',
+  age: '16'
+}, {
+  id: 4,
+  name: 'Garik',
+  age: '27'
+}];
 let subjects = ['Python', 'Java'];
 let classes = ['10A', '12B'];
 let lastId = 4;
@@ -46,7 +45,13 @@ app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
   extended: true
 }));
 
-app.get('/students', (req, res) => res.send(students));
+app.get('/students', (req, res) => {
+  if (req.query.grade) {
+    res.send(students.filter((student) => student.grade === req.query.grade));
+  } else {
+    res.send(students);
+  }
+});
 app.get('/students/:id', (req, res) => {
   let student = students.find(function (element) {
     return element.id == req.params.id;
@@ -56,18 +61,56 @@ app.get('/students/:id', (req, res) => {
   }
   res.send(student);
 });
-app.post('/addStudent', (req, res) => {
+app.post('/addStudent',
+[
+  check('firstname', 'Firstname is required').exists(),
+  check('firstname', 'Firstname must contain minimum 3 letter').isLength({
+    min: 3
+  }),
+  check("firstname", "Only the first letter must be upper").matches("^[A-Z][-a-z]+$"),
+
+  check('lastname', 'Lastname is required').exists(),
+  check('lastname', 'Lastname must contain minimum 3 letter').isLength({
+    min: 3
+  }),
+  check("lastname", "Only the first letter must be upper").matches("^[A-Z][-a-z]+$"),
+
+  check('age', 'Age is required').exists(),
+  check('age', 'Age must be greater than 6 and less than 30.').isInt({
+    gt: 6,
+    lt: 30
+  }),
+
+  check('email', 'Email is required').exists(),
+  check('email').isEmail(),
+
+  check('gender', 'Gender is required').exists(),
+  check('gender', 'Gender can be male or female').isIn(['male', 'female']),
+
+  oneOf([[
+    check('gender').equals('male'),
+    check("carNumber", "Ivalid car number").matches("^[0-9]{2}[A-Z]{2}[0-9]{3}$"),
+  ], [
+    check('gender').equals('female'),
+  ]]),
+
+], 
+function (req, res)  {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
   newStudent = req.body;
   newStudent.id = ++lastId;
   students.push(newStudent);
-  res.send(`${newStudent.name} successfully added`);
+  res.send(`${newStudent.firstname} successfully added`);
 });
 app.delete('/deleteStudent/:id', (req, res) => {
   let message = `There is no student for id ${req.params.id}`;
   for (let i = 0; i < students.length; i++) {
     if (students[i].id.toString() === req.params.id) {
       students.splice((i - 1), 1);
-      message = `Student ${req.params.id} successfully deleted`;
+      message = `Student ${req.params.firstname} successfully deleted`;
       break;
     }
   }
@@ -80,7 +123,7 @@ app.put('/editStudent/:id', (req, res) => {
       newStudent = req.body;
       newStudent.id = req.params.id;
       students[i] = newStudent;
-      message = `Student ${req.params.id} successfully edited`;
+      message = `Student ${req.params.firstname} successfully edited`;
       break;
     }
   }
@@ -108,7 +151,7 @@ app.delete('/deleteTeacher/:id', (req, res) => {
   for (let i = 0; i < teachers.length; i++) {
     if (teachers[i].id.toString() === req.params.id) {
       teachers.splice((i - 1), 1);
-      message = `Teacher ${req.params.id} successfully deleted`;
+      message = `Teacher ${req.params.name} successfully deleted`;
       break;
     }
   }
@@ -121,7 +164,7 @@ app.put('/editTeacher/:id', (req, res) => {
       newTeacher = req.body;
       newTeacher.id = req.params.id;
       teachers[i] = newTeacher;
-      message = `Teacher ${req.params.id} successfully edited`;
+      message = `Teacher ${req.params.name} successfully edited`;
       break;
     }
   }
