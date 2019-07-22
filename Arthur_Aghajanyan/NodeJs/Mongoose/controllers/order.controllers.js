545e5574233ebf3e019b361f650dd2fb803522ea
Arthur_Aghajanyan <arthur.aghajanyan.1994@mail.ru>
2019-07-22 17:00:56 +0400

    Added mongoose homework

---
+++ Arthur_Aghajanyan/NodeJs/Mongoose/controllers/order.controllers.js
@@ -0,0 +1,32 @@
+const Order = require('../models/order.models');
+
+function addOrder(order) {
+    const newOrder = new Order(order);
+    return newOrder.save();
+}
+
+function findOrders() {
+    return Order.find({}).exec();
+}
+
+function findOrdersById(id) {
+    return Order.findById(id).exec();
+}
+
+function deleteOrder(id) {
+    return Order.deleteOne({
+        _id: id
+    }).exec();
+}
+
+function updateOrder(id, info) {
+    return Order.updateOne({
+        _id: id
+    }, info, {runValidators: true}).exec();
+}
+
+module.exports.addOrder = addOrder;
+module.exports.findOrders = findOrders;
+module.exports.findOrdersById = findOrdersById;
+module.exports.deleteOrder = deleteOrder;
+module.exports.updateOrder = updateOrder;
