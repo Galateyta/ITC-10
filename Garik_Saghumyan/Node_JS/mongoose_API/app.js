13cd6cb365bce8c2382b56df3b0aee7584603e95
GarikSaghumyan <saghumyangarik@gmail.com>
2019-07-24 18:06:03 +0400

    Author : Garik Saghumyan : added env variables

--- Garik_Saghumyan/Node_JS/mongoose_API/app.js
+++ Garik_Saghumyan/Node_JS/mongoose_API/app.js
@@ -4,15 +4,16 @@ const users = require('./routers/user.rout');
 const orders = require('./routers/order.rout');
 const products = require('./routers/product.rout');
 const app = express();
+const dotenv = require('dotenv')
+dotenv.config();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({
     extended: false
 }))
 app.use(bodyParser.json());
-mongoose.connect('mongodb://localhost:27017/shop', {useNewUrlParser: true});
+mongoose.connect('mongodb://' + process.env.DB_HOST + ':' + process.env.DB_PORT + '/'+ process.env.DB_NAME , {useNewUrlParser: true});
 app.listen(10000);
 app.use('/users' , users);
 app.use('/orders', orders);
 app.use('/products', products);
-
 module.exports = app;
--- Garik_Saghumyan/Node_JS/mongoose_API/models/order.model.js
+++ Garik_Saghumyan/Node_JS/mongoose_API/models/order.model.js
@@ -11,12 +11,9 @@ const Order = mongoose.model('order', new mongoose.Schema({
         min: 1,
         default:1
     },
-    createAt: {
-        type: Date,
-        default: Date.now(),
-        index: { expireAfterSeconds: 2592000 } 
-    },
-    products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'product' }]
-}));
+   
+    products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'product' }],
+}, {timestamps: true}).index({createdAt: 1},{expireAfterSeconds: 2592000}));
+
 
 module.exports = Order;
