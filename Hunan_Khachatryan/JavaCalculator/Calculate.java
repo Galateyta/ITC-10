787d725f1f8f728db7753780b00dc93f372c5e0c
HunanKhachatryan <hunank@mail.ru>
2019-07-31 11:45:04 +0400

    -Fixed bugs

--- Hunan_Khachatryan/JavaCalculator/Calculate.java
+++ Hunan_Khachatryan/JavaCalculator/Calculate.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -6,149 +7,140 @@ import java.nio.file.Paths;
 import java.util.Stack;
 
 public class Calculate {
-    public static  void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         StringBuilder sb = new StringBuilder();
-        String filename = "/home/hunan/IdeaProjects/Calculate/src/main/java/file.txt";
+        String filename = "/src/resources/file.txt";
 
-            try (BufferedReader br = Files.newBufferedReader(Paths.get(filename))) {
-                String line;
+        try (BufferedReader br = Files.newBufferedReader(Paths.get(filename))) {
+            String line;
             while ((line = br.readLine()) != null) {
                 sb.append(line).append("\n");
             }
 
-        } catch (IOException e) {
-            System.err.format("IOException: %s%n", e);
         }
-
 
         System.out.println(sb);
 
         String[] tmp = sb.toString().split("=");
-
         String leftPart = tmp[0];
-        Calculate  expression = new Calculate();
+        Calculate expression = new Calculate();
 
-        boolean isBalanced = expression.AreParanthesisBalanced(leftPart);
+        boolean isBalanced = expression.areParanthesisBalanced(leftPart);
 
         if (isBalanced) {
             double leftResult = expression.evalExpression(leftPart);
-            if ( tmp.length != 1) {
-                if( tmp[1].length() > 1){
-                    double rigthPart = Double.parseDouble(tmp[1]) ;
-                    if(rigthPart == leftResult){
+            if (tmp.length != 1) {
+                if (tmp[1].length() > 1) {
+                    double rigthPart = Double.parseDouble(tmp[1]);
+                    if (rigthPart == leftResult) {
                         System.out.println("The expression is true");
 
-                    }else {
+                    } else {
                         System.out.println("The expression is false");
                     }
-                }else {
+                } else {
                     System.out.println("Exeption is not validate");
                 }
 
-            }else {
+            } else {
                 double result = expression.evalExpression(leftPart);
-                expression.writeFile(filename,result);
+                expression.writeInFile(filename, result);
                 System.out.println(result);
 
             }
-        }else {
+        } else {
             System.out.println("In expression the paranthes is not balanced");
 
         }
     }
-    public  void writeFile(String filename, Double text){
-        try{
-            FileWriter fWrite = new FileWriter(filename,true);
-            fWrite.write(" = " + text);
-            fWrite.close();
 
-        }catch(Exception e){
-            System.out.println(e);
+    public void writeInFile(String filename, Double result) throws IOException {
+        try (FileWriter fWrite = new FileWriter(filename, true)) {
+            fWrite.write(" = " + result);
         }
     }
 
-
-    private int priority(char op){
-
-        if(op == '+'||op == '-')
+    private int priority(char op) {
+        switch (op) {
+        case '+':
+        case '-':
             return 1;
-        if(op == '*'||op == '/')
+        case '*':
+        case '/':
             return 2;
-        return 0;
+        default:
+            return 0;
+        }
     }
 
-    private double oper(double a, double b, char op){
-        switch(op){
-            case '+': return a + b;
-            case '-': return a - b;
-            case '*': return a * b;
-            case '/': return a / b;
-            default: return 0;
+    private double oper(double a, double b, char op) {
+        switch (op) {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        case '/':
+            return a / b;
+        default:
+            return 0;
         }
 
     }
 
-
-    public boolean AreParanthesisBalanced(String str)
-    { int count = 0;
-        for(int i = 0; i < str.length(); i++)
-        {
-            if(str.charAt(i) == '(' && str.charAt(i+1) == ')' )
-            {
+    public boolean areParanthesisBalanced(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '(' && str.charAt(i + 1) == ')') {
                 return false;
             }
-            if(str.charAt(i) == '(' )
-            {
+            if (str.charAt(i) == '(') {
                 count++;
             }
-            if(str.charAt(i) == ')')
-            {
+            if (str.charAt(i) == ')') {
                 count--;
             }
 
-            if (count < 0 )
-            {
-                return  false;
+            if (count < 0) {
+                return false;
             }
 
         }
         if (count > 0) {
             return false;
         }
-            return true;
+        return true;
 
     }
 
-    public double evalExpression(String str){
+    public double evalExpression(String str) {
         int i;
-        Stack <Double>  numbers = new Stack<Double>();
-        Stack <Character>  operations = new Stack<Character>();
+        Stack<Double> numbers = new Stack<Double>();
+        Stack<Character> operations = new Stack<Character>();
 
-        for(i = 0; i < str.length(); i++){
+        for (i = 0; i < str.length(); i++) {
 
-            if(str.charAt(i)== ' '){
+            if (str.charAt(i) == ' ') {
                 continue;
             }
 
-            if(str.charAt(i) == '('){
+            if (str.charAt(i) == '(') {
                 operations.push(str.charAt(i));
             }
 
-            else
-            if(Character.isDigit(str.charAt(i))){
+            else if (Character.isDigit(str.charAt(i))) {
                 double val = 0;
 
-                while(i < str.length() && Character.isDigit(str.charAt(i)))
-                {
-                    val = (val*10) + (str.charAt(i)-'0');
+                while (i < str.length() && Character.isDigit(str.charAt(i))) {
+                    val = (val * 10) + (str.charAt(i) - '0');
                     i++;
                 }
 
                 numbers.push(val);
-            }else if(str.charAt(i) == ')') {
+            } else if (str.charAt(i) == ')') {
 
-                while(!operations.isEmpty() && operations.peek() != '(')
-                {
+                while (!operations.isEmpty() && operations.peek() != '(') {
                     double num2 = numbers.peek();
                     numbers.pop();
 
@@ -161,10 +153,8 @@ public class Calculate {
                     numbers.push(oper(num1, num2, op));
                 }
                 operations.pop();
-            }
-            else {
-                while(!operations.isEmpty() && priority(operations.peek())
-                        >= priority(str.charAt(i))){
+            } else {
+                while (!operations.isEmpty() && priority(operations.peek()) >= priority(str.charAt(i))) {
                     double num2 = numbers.peek();
                     numbers.pop();
 
@@ -179,7 +169,7 @@ public class Calculate {
                 operations.push(str.charAt(i));
             }
         }
-        while(!operations.isEmpty()){
+        while (!operations.isEmpty()) {
             double num2 = numbers.peek();
             numbers.pop();
 
@@ -196,5 +186,3 @@ public class Calculate {
     }
 
 }
-
-
