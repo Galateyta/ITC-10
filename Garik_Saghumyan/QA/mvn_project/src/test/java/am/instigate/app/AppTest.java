8ff404b471c0d7ccff80be55deb25aa5f9240b42
GarikSaghumyan <saghumyangarik@gmail.com>
2019-08-01 18:00:59 +0400

    Author : Garik Saghumyan : modified testing

--- Garik_Saghumyan/QA/mvn_project/src/test/java/am/instigate/app/AppTest.java
+++ Garik_Saghumyan/QA/mvn_project/src/test/java/am/instigate/app/AppTest.java
@@ -2,8 +2,6 @@ package am.instigate.app;
 
 import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -15,22 +13,36 @@ public class AppTest
     @Rule
     public ExpectedException exception = ExpectedException.none();
 
+    // input of function is correct and expected answer is correct
     @Test
-    public void shouldAnswerWithTrue() {
+    public void shouldAnswerCorrect() {
         Calculator calculator = new Calculator();
-        assertEquals(7.0, calculator.doTheShuntingYard("((5 + 2) * 2)/2") );
+        assertEquals("shouldAnswerCorrect test function failed",7.0, calculator.doTheShuntingYard("((5 + 2) * 2)/2") );
     }
+
+    // input of function is correct but expected answer is wrong
+    @Test
+    public void shouldAnswerNotCorrect() {
+        Calculator calculator = new Calculator();
+        assertNotEquals("shouldAnswerNotCorrect test function failed",0.0, calculator.doTheShuntingYard("5 + 2 * 4/2") );
+    }
+
+    // input of function is empty and execpted exeption is IllegalArgumentException
+    @Test
+    public void emptyExpression() {
+        exception.expect(IllegalArgumentException.class);
+        exception.expectMessage("Empty expression or null");
+        Calculator calculator = new Calculator();
+        calculator.doTheShuntingYard("");
+    }
+
+    // input of function is empty string and execpted exeption is IllegalArgumentException
     @Test
     public void throwsArithmeticExeption() {
+        exception.expect(ArithmeticException.class);
         exception.expectMessage("Division by 0");
         Calculator calculator = new Calculator();
         calculator.doTheShuntingYard("((5 + 2) * 2)/0");
-    }
-
-    @Test
-    public void shouldAnswerWithError() {
-        Calculator calculator = new Calculator();
-        assertNotEquals(0.0, calculator.doTheShuntingYard("5 + 2 * 4/2") );
     }
 
     @Test
@@ -42,7 +54,15 @@ public class AppTest
 
     }
     @Test
-    public void testMain() throws IOException {
+    public void invalidOperator() {
+        exception.expect(IllegalArgumentException.class);
+        exception.expectMessage("Invalid Expression:5^7");
+        Calculator calculator = new Calculator();
+        calculator.doTheShuntingYard("5^7");
+
+    }
+    @Test
+    public void checkMainWithCorrectFile() throws IOException {
         System.out.println("main");
         Calculator calculator = new Calculator();
         File file = new File("src/resources/file.txt");
@@ -55,10 +75,10 @@ public class AppTest
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
             writer.write(tmp[0] + '=' + result);
         }
-        assertEquals(12.0, result );
+        assertEquals( "checkMainWithCorrectFile test function failed",12.0, result );
     }
     @Test
-    public void testMain1() throws IOException {
+    public void checkMainWithInvalidFile() throws IOException {
         exception.expect(IllegalArgumentException.class);
         Calculator calculator = new Calculator();
         File file = new File("src/resources/file1.txt");
@@ -70,6 +90,15 @@ public class AppTest
         double result = calculator.doTheShuntingYard(tmp[0]);
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
             writer.write(tmp[0] + '=' + result);
+        }
+    }
+
+    @Test
+    public void checkMainWithoutFilePath() throws IOException {
+        exception.expect(FileNotFoundException.class);
+        exception.expectMessage("No such file or directory");
+        File file = new File("");
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
         }
     }
 }
