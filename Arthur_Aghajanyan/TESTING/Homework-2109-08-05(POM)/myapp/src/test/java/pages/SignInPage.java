73155a04e294e2132b766edd7736e73ac919faeb
Arthur Aghajanyan <arthur.aghajanyan.1994@mail.ru>
2019-08-06 01:50:40 +0400

    -Author: Arthur Aghajanyan: Updated project structure to page object model

---
+++ Arthur_Aghajanyan/TESTING/Homework-2109-08-05(POM)/myapp/src/test/java/pages/SignInPage.java
@@ -0,0 +1,60 @@
+package pages;
+import constants.SignInConstant;
+import helperFunctions.HelperFunctions;
+import org.openqa.selenium.By;
+import tests.*;
+
+
+import org.openqa.selenium.WebElement;
+
+public class SignInPage {
+    private  WebElement signInText;
+    private WebElement forgotPasswordButton;
+    private WebElement password;
+    private static SignInPage pageInstance;
+
+    public SignInPage(){
+        forgotPasswordButton = BasePage.driver.findElement(By.xpath(SignInConstant.forgotPasswordButton));
+        signInText = BasePage.driver.findElement(By.xpath(SignInConstant.signInText));
+        password = BasePage.driver.findElement(By.id(SignInConstant.password));
+
+    }
+
+    public static SignInPage getInstance() {
+        HelperFunctions.sleepSpecificTime(2);
+        HelperFunctions.waitToElement(BasePage.driver, 7, SignInConstant.signInText);
+        if (pageInstance == null) {
+            pageInstance = new SignInPage();
+        }
+        return pageInstance;
+    }
+
+    public WebElement getSignInText() {
+        return signInText;
+    }
+
+    public void setSignInText(WebElement signInText) {
+        this.signInText = signInText;
+    }
+
+    public WebElement getForgotPasswordButton() {
+        return forgotPasswordButton;
+    }
+
+    public void setForgotPasswordButton(WebElement forgotPasswordButton) {
+        this.forgotPasswordButton = forgotPasswordButton;
+    }
+
+    public WebElement getPassword() {
+        return password;
+    }
+
+    public void setPassword(WebElement password) {
+        this.password = password;
+    }
+
+    public ForgotPasswordPage clickForgotPasswordButton(){
+        forgotPasswordButton.click();
+        return ForgotPasswordPage.getInstance();
+    }
+}
